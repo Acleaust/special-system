ac22e3a080b57b6154ba82775ff65db3da46f593
Acleaust <acleaust@gmail.com>
2018-01-21 01:42:03 +0200

    New version

--- js/bg.js
+++ js/bg.js
@@ -1,22 +1,18 @@
 var granimInstance = new Granim({
     element: '#canvas-basic',
     name: 'basic-gradient',
-    direction: 'left-right', // 'diagonal', 'top-bottom', 'radial'
+    direction: 'left-right', // 'diagonal', 'top-bottom', 'radial', 'left-right'
     opacity: [1, 1],
     isPausedWhenNotInView: true,
     states : {
         "default-state": {
             gradients: [
-                ['#2574A9', '#4183D7'],
-                ['#446CB3', '#22313F'],
-                ['#34495E', '#3498DB']
+                ['#005a99', '#0071c1'],
+                ['#0071c1', '#22313F'],
+                ['#0071c1', '#005a99']
             ],
             transitionSpeed: 7000
 
         }
     }
 });
-
-//['#2574A9', '#1E8BC3'],
-//['#59ABE3', '#89C4F4'],
-//['#E4F1FE', '#4183D7']
